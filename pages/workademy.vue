25a5a396d47e54f9279569d42e690683865ea86f
maria <maria.recska@gmail.com>
2022-12-02 14:27:27 +0100

    workademy link added

--- pages/workademy.vue
+++ pages/workademy.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="flex-col space-y-4 bg-black h-screen px-6 py-12 text-white">
-    <h2 class="font-exo text-4xl" ref="title">Workademy</h2>
-    <h4 class="font-exo text-3xl">23.08.2021-15.10.2022</h4>
+  <div
+    class="flex-col space-y-4 md:space-y-8 h-screen px-6 md:px-8 py-12 text-white max-w-sm md:max-w-lg ml:max-w-5xl"
+  >
+    <h2 class="font-exo text-5xl" ref="title">Workademy</h2>
+    <h4 class="font-exo text-3xl">10.03.2020-04.05.2020</h4>
     <p class="font-exo text-xl mt-8">
       The internship was organized as part of the Wild Code School training
-      program. We developed the admin part of the Smart Course Builder
-      application, using React, Redux and styled components. I was coding
-      smaller components in the beginning (buttons, home page, clickable
-      sidebar) and then I was responsible for theming (light and dark mode)
-      using React hooks and Context API
+      program. <br />
+      We developed the admin part of the Smart Course Builder application, using
+      React, Redux and styled components. <br />I was coding smaller components
+      in the beginning (buttons, home page, clickable sidebar) and then I was
+      responsible for theming (light and dark mode) using React hooks and
+      Context API
     </p>
+    <div class="mt-8">
+      <a class="font-exo text-3xl" href="https://github.com/chudaol/workademy"
+        >GitHub Link</a
+      >
+    </div>
   </div>
 </template>
