02d4793a1a67a74daedc49fb6198d01146dd1fe7
maria <maria.recska@gmail.com>
2022-12-21 19:35:26 +0100

    texts and metadata improved

--- pages/projects/accentro.vue
+++ pages/projects/accentro.vue
@@ -12,15 +12,20 @@
     <h4 class="font-exo text-2xl md:text-3xl">01.07.2021-30.10.2022</h4>
     <p class="font-exo text-xl mt-8">
       This is the website of a Berlin-based real estate company. It was built
-      with React and next.js frameworks, combined with Typescript.
-      <br />The user can search and filter for apartments, register, upload
+      with React and Next.js frameworks, combined with Typescript.
+      <br />The users can search and filter for apartments, register, upload
       documents, and send messages to the brokers. It also displays relevant
       informations and news about the property market in Berlin.<br />
-      worked on different features (UI improvements, user account creation,
-      document upload, news display and chart display with the help of
-      Contentful).<br />
-      I implemented several end-to-end test with Cypress, and I wrote nearly the
-      complete documentation of the project in the internal GitHub Wiki.
+      I worked on smaller UI improvements and fixed bugs in the beginning. Then
+      I participated in bigger features, for example user account creation,
+      document upload, property news, lexicon, newsletters and other various
+      tasks that involved working creating new content models and queries in the
+      company's Contentful CMS.<br />
+      I optimized the website's SEO by implementing JSON-LD - structured data
+      markup that helps Google Search Engine to understand the content of a
+      page.<br />
+      I also created several end-to-end test with Cypress, and I wrote nearly
+      the complete documentation of the project in the internal GitHub Wiki.
     </p>
     <div class="flex h-24 py-8 md:py-4 ml:max-w-md">
       <a href="https://accentro.de/" target="_blank">
--- pages/projects/proprate.vue
+++ pages/projects/proprate.vue
@@ -13,11 +13,11 @@
     <p class="font-exo text-xl mt-8">
       I worked on this project for a short time, when the workload was less by
       ACCENTRO. PropRate is a free tool to analyze real estate.<br />The
-      webapplication was built with React and next.js frameworks, using also
+      webapplication was built with React and Next.js frameworks, using also
       Typescript as a language. The user can rate and compare apartments for
-      sale within Germany.<br />I worked on smaller atures: UI improvements,
-      user account creation and password check. I implemented end-to-end test
-      with Cypress.
+      sale within Germany.<br />I worked on smaller features: UI improvements,
+      user account creation and password check. I implemented some end-to-end
+      tests with Cypress.
     </p>
     <div class="flex h-24 py-8 md:py-4 ml:max-w-md">
       <a href="https://proprate.de/" target="_blank">
